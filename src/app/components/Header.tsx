14093d0309f6fea240d401cc7d0a1859738f666f
dalipdivaker <dalipdivakar@gmail.com>
2025-05-27 11:02:32 +0530

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -11,7 +11,7 @@ const navLinks = [
   { name: "Home", href: "/" },
   { name: "About Us", href: "/about" },
   { name: "Products", href: "/products" },
-  { name: "Careers", href: "/careers" },
+  { name: "Careers", href: "/contact" },
   { name: "Contact", href: "/contact" },
 ];
 
