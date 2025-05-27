a2b93e2f0f7b7b56f9cb3072cb83d450bcb2680f
dalipdivaker <dalipdivakar@gmail.com>
2025-05-27 11:56:15 +0530

    Update page.tsx

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,12 +1,13 @@
-import ContactSection from '../components/career' // Adjust path based on your structure
-
+ // Adjust path based on your structure
+import ContactUs from '../components/contact'
 export default function CareerPage() {
   return (
     <main className="">
       {/* Other sections like Hero, Open Positions etc. */}
 
       {/* Contact Section at the bottom */}
-      <ContactSection />
+      
+      <ContactUs/>
     </main>
   )
 }
